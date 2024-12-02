a5feb18cd5507f3d3b84c67ce8516770b0e83416
Morten Kolstad <morten.kolstad@bekk.no>
2024-12-02 09:19:33 +0100

    day2

--- src/lib/Solutions.hs
+++ src/lib/Solutions.hs
@@ -3,6 +3,7 @@ module Solutions where
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Day.Day01 qualified
+import Day.Day02 qualified
 
 import DayVersion
 import Utils ((=:))
@@ -11,7 +12,7 @@ solutions :: Map DayVersion (String -> IO ())
 solutions =
   Map.fromList
     [ "1" =: Day.Day01.run
-    -- , "2" =: Day.Day02.run
+    , "2" =: Day.Day02.run
     -- , "3" =: Day.Day03.run
     -- , "4" =: Day.Day04.run
     -- , "5" =: Day.Day05.run
