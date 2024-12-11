0d80bbd610cefe711b45f424ba207bc7ead568c5
Morten Kolstad <morten.kolstad@bekk.no>
2024-12-11 22:31:15 +0100

    small fixes

--- src/lib/Day/Day11.hs
+++ src/lib/Day/Day11.hs
@@ -3,6 +3,7 @@ module Day.Day11 (run) where
 import Control.Arrow ((>>>))
 import Control.Lens (ifoldMapByOf, ifolded, (<&>))
 import Control.Monad (void)
+import Data.Foldable (Foldable (fold))
 import Data.IntMap.Strict (IntMap)
 import Data.IntMap.Strict qualified as IntMap
 import Test.HUnit ((@=?))
@@ -17,25 +18,26 @@ getStoneCounts :: [Int] -> _
 getStoneCounts xs = iterate calculateStones stoneMap <&> sum
  where
   calculateStones = ifoldMapByOf ifolded (<+>) mempty blink
-  stoneMap = foldr (<+>) mempty $ fmap (`IntMap.singleton` 1) xs
+  stoneMap = foldMap (`IntMap.singleton` 1) xs
 
 blink :: Int -> Int -> IntMap Int
 blink 0 count = IntMap.singleton 1 count
 blink stoneNumber count
-  | let d = digitCount stoneNumber
+  | let d = digits stoneNumber
   , even d
   , let (a, b) = splitNumber d stoneNumber =
       IntMap.singleton a count <+> IntMap.singleton b count
 blink stoneNumber count = IntMap.singleton (stoneNumber * 2024) count
 
-digitCount :: Int -> Int
-digitCount = succ . floor . logBase 10 . fromIntegral
+digits :: Int -> Int
+digits 0 = 1
+digits n = length $ takeWhile (> 0) $ iterate (`div` 10) n
 
 splitNumber :: Int -> Int -> (Int, Int)
-splitNumber digitCount n = (n `div` divisor, n `mod` divisor)
+splitNumber digitNumber n = (n `div` divisor, n `mod` divisor)
  where
-  halfCount = digitCount `div` 2
-  divisor = 10 ^ (digitCount - halfCount)
+  halfCount = digitNumber `div` 2
+  divisor = 10 ^ (digitNumber - halfCount)
 
 run :: String -> IO ()
 run input = void $ do
